1c6e9597d4cb6c7b643b1e4afa2ee5b52dd15189
joao <jlucasoc@hotmail.com>
2021-10-03 21:43:39 -0300

    🚀 Create first pages and routes

--- consumirApi/src/components/Header/index.js
+++ consumirApi/src/components/Header/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
-
   return (
     <Nav>
       <Link to="/">
-        <FaHome size={botaoClicado ? 24 : 50} />
+        <FaHome size={24} />
       </Link>
-      <Link to="/user">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
       <Link to="/login">
